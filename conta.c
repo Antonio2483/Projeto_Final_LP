8a7603e5b2d68ef47cf38b0d788f1eb2c84613c3
Antonio2483 <antoniolsferreira2015@gmail.com>
2022-12-02 16:44:36 -0300

    Menu e arquivos adicionados

---
+++ conta.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "produto.h"
+
+struct Conta{
+    int numero;
+    char cliente[51];
+    int especial;
+    float saldo;
+};
+
---
+++ conta.h
@@ -0,0 +1,6 @@
+#ifndef CONTA_H_INCLUDED
+#define CONTA_H_INCLUDED
+
+typedef struct Conta * p_conta;
+
+#endif // CONTA_H_INCLUDED
